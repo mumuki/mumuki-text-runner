014c715d29d449876c063d9a0cf204b350761335
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-20 13:57:12 -0300

    Fixing tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,11 +4,6 @@ require 'active_support/all'
 
 describe 'integration test' do
   let(:bridge) { Mumukit::Bridge::Runner.new('http://localhost:4567') }
-  let(:request) do
-    {content: ' Lorem ipsum ',
-     test: "- name: 'test1'\n  postconditions:\n    equal: 'Lorem ipsum'",
-     extra: ''}
-  end
 
   before(:all) do
     @pid = Process.spawn 'rackup -p 4567', err: '/dev/null'
@@ -17,17 +12,30 @@ describe 'integration test' do
 
   after(:all) { Process.kill 'TERM', @pid }
 
-  xit 'answers a valid hash when submission passes' do
-    response = bridge.run_tests!(request)
+  it 'answers a valid hash when submission fails' do
+    response = bridge.run_tests!(content: ' Lorem ipsum ',
+                                 test: "- name: 'test1'\n  postconditions:\n    equal: 'Lorem ipsum'",
+                                 extra: '')
 
-    expect(response[:status]).to eq(:passed)
+    expect(response).to eq response_type: :structured,
+                           test_results: [{title: 'test1', status: :passed, result: nil}],
+                           status:  :passed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: ''
   end
 
-  xit 'answers a valid hash when submission fails' do
-    response = bridge.run_tests!(request)
+  it 'answers a valid hash when submission passes' do
+    response = bridge.run_tests!(content: ' Dolor amet ',
+                                 test: "- name: 'test2'\n  postconditions:\n    equal: 'Lorem ipsum'",
+                                 extra: '')
 
-    expect(response[:result]).to include 'is not the right value'
-    expect(response[:status]).to eq :failed
+    expect(response).to eq response_type: :structured,
+                           test_results: [{title: 'test2', status: :failed, result: '**Dolor amet** is not the right value.'}],
+                           status:  :failed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: ''
   end
 
 end
