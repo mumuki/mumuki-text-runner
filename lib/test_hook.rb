3a255535dc66538e92ac024bd0173aadf23cc82d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-10-12 16:51:27 -0300

    Delegating some methods

--- lib/test_hook.rb
+++ lib/test_hook.rb
@@ -18,17 +18,24 @@ class TextTestHook < Mumukit::Hook
 
   def parse_test(tests)
     parsed_test = YAML.load(tests)
-
     if parsed_test.is_a? Array
-      parsed_test.map { |example| example.deep_symbolize_keys }
+      parse_multi_scenario_test(parsed_test)
     else
-      [{name: 'test',
-        postconditions: {equal: {
-          expected: parsed_test['equal'],
-          ignore_case: parsed_test['ignore_case'].present?,
-          ignore_whitespace: parsed_test['ignore_whitespace'].present?}}
-       }]
+      parse_single_scenario_test(parsed_test)
     end
 
+  end
+
+  def parse_single_scenario_test(parsed_test)
+    [{name: 'test',
+      postconditions: {equal: {
+        expected: parsed_test['equal'],
+        ignore_case: parsed_test['ignore_case'].present?,
+        ignore_whitespace: parsed_test['ignore_whitespace'].present?}}
+     }]
+  end
+
+  def parse_multi_scenario_test(parsed_test)
+    parsed_test.map { |example| example.deep_symbolize_keys }
   end
 end
