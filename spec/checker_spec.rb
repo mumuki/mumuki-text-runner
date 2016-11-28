1de98967a4811bd08c4abaeea1c4e3aea17a673d
Charlyzzz <erwincdl@gmail.com>
2016-11-28 16:12:44 -0300

    Checker macro for implementing checks

--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -2,13 +2,30 @@ require_relative './spec_helper'
 
 describe TextChecker do
   let(:checker) { TextChecker.new }
+  let(:test) { { source: 'Lorem ipsum dolor sit amet' } }
 
   describe '#check_equals' do
     let(:result) { checker.check_equal(test, config) }
-    let(:test) { { source: 'Lorem ipsum dolor sit amet' } }
 
     context 'when it passes' do
       let(:config) { { expected: 'lorem ipsum dolor sit amet', ignore_case: true } }
+
+      it { expect { result }.not_to raise_error }
+    end
+
+    context 'when it fails' do
+      let(:config) { { expected: 'Hey!' } }
+
+      it { expect { result }.to raise_error Mumukit::Metatest::Failed }
+    end
+  end
+
+  describe '#check_contains' do
+    let(:result) { checker.check_contain(test, config) }
+    let(:test) { { source: 'Lorem ipsum dolor sit amet' } }
+
+    context 'when it passes' do
+      let(:config) { { expected: 'IPSUM dolOr', ignore_case: true } }
 
       it { expect { result }.not_to raise_error }
     end
