3beb531f9f2c438311724678bba9977684d86f2a
Franco Bulgarelli <franco@mumuki.org>
2018-10-02 11:23:11 -0300

    Testing options in checker spec

--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -31,4 +31,18 @@ describe TextChecker do
       it { is_expected.to eq ['test', :failed, '**world** is not the right value.'] }
     end
   end
+
+  context 'when using ignore space flags' do
+    let(:assertions) {{ equal: {expected: 'hello world', ignore_whitespace: true} }}
+
+    context 'when pass' do
+      let(:source) { '  hello    world' }
+      it { is_expected.to eq ['test', :passed, nil] }
+    end
+
+    context 'when do not pass' do
+      let(:source) { 'hey jude' }
+      it { is_expected.to eq ['test', :failed, '**hey jude** is not the right value.'] }
+    end
+  end
 end
